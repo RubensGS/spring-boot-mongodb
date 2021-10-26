ee694ce8c1982c45ef43c68312e580f947b25b39
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-10-26 20:54:47 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/workshop/demo/domain/User.java
+++ src/main/java/com/workshop/demo/domain/User.java
@@ -1,6 +1,8 @@
 package com.workshop.demo.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
@@ -9,9 +11,11 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "user")
 public class User implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/workshop/demo/resources/UserResource.java
+++ src/main/java/com/workshop/demo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.workshop.demo.resources;
 
 import com.workshop.demo.domain.User;
+import com.workshop.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,15 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+    //Serve para injeção da classe
+    @Autowired
+    private UserService service;
 
     //@GetMapping - Dá na mesma utilizar um ou outro
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria Silva", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
