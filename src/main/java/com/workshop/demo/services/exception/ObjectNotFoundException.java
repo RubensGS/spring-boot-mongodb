403783ee7426df0ae6d80842f24d5672033b5e76
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-10-26 21:26:26 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/workshop/demo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.workshop.demo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException( String msg) {
+        super(msg);
+    }
+}
