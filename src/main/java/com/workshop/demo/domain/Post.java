895fdce84eca3c86af1c763a6801db5caa994d81
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-17 13:15:50 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/workshop/demo/domain/Post.java
+++ src/main/java/com/workshop/demo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.workshop.demo.domain;
 
+import com.workshop.demo.dto.AuthorDTO;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,11 +8,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 import java.util.Date;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 @Document
 public class Post implements Serializable {
 
@@ -21,6 +19,6 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
 }
